4ad4e2926c43d274c83a56b8b158215f0f8034b7
arimaji98 <ari.maji.98@gmail.com>
2024-06-03 00:48:05 +0530

    video title

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -9,8 +9,7 @@ const VideoBackground = ({ movieId }) => {
   return (
     <div>
       <iframe
-        width="560"
-        height="315"
+        className="w-screen aspect-video"
         src={"https://www.youtube.com/embed/" + movieTrailer?.key}
         title="YouTube video player"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
--- src/components/VideoTitle.js
+++ src/components/VideoTitle.js
@@ -3,12 +3,16 @@ import React from "react";
 const VideoTitle = (props) => {
   const { title, overview } = props;
   return (
-    <div className="pt-36 px-12">
+    <div className="pt-36 px-12 absolute w-screen aspect-video text-white bg-gradient-to-r from-black">
       <h1 className="text-4xl font-bold">{title}</h1>
       <p className="py-6 text-sm w-1/4">{overview}</p>
       <div>
-        <button className=" bg-gray-400 px-5 py-1 rounded bg-opacity-50"> ▶ Play</button>
-        <button className="mx-2 bg-gray-400 px-5 py-1 rounded bg-opacity-50">More Info.</button>
+        <button className=" bg-gray-400 px-5 py-1 rounded bg-opacity-50">
+          ▶ Play
+        </button>
+        <button className="mx-2 bg-gray-400 px-5 py-1 rounded bg-opacity-50">
+          More Info.
+        </button>
       </div>
     </div>
   );
